a19e3c9a8f7c53e1ee510549579c201512848e93
TAEHYUN LEE <yeourain@gmail.com>
2023-05-12 22:31:33 +0900

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,6 @@ import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 
 export default function App() {
-  // return (<MainPage/>).
+  // return (<MainPage/>)
   return (<AboutPage/>)
 }
